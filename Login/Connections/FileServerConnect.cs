be094bacd408b4b790b81ce13e951c63eaa80a68
Kefido <kefido@outlook.de>
2022-12-25 23:59:35 +0100

    i made an ftp connection now i have to write it over so the user can choose his file also i need to comment things and investigate how the block of code really works to fully understand it
    im on a good way

---
+++ Login/Connections/FileServerConnect.cs
@@ -0,0 +1,48 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.FtpClient;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Login
+{
+    internal class FileServerConnect
+    {
+        //buildFTPConnection
+        //client.Credentials = new NetworkCredential("ftpserveruser", "mygrind400");
+        //            string url = "ftp://192.168.2.36/abc";
+        //            client.UploadFile(url, @"C:\Users\kefid\OneDrive\Bilder\dondacover.png");
+
+        public string UploadFile(string FtpUrl, string fileName, string userName, string password, string
+     UploadDirectory = "")
+        {
+            string PureFileName = new FileInfo(fileName).Name;
+            String uploadUrl = String.Format("{0}{1}/{2}", FtpUrl, UploadDirectory, PureFileName);
+            FtpWebRequest req = (FtpWebRequest)FtpWebRequest.Create(uploadUrl);
+            req.EnableSsl = true;
+            ServicePointManager.ServerCertificateValidationCallback =
+                      (s, certificate, chain, sslPolicyErrors) => true;
+            req.Proxy = null;
+            req.Method = WebRequestMethods.Ftp.UploadFile;
+            req.Credentials = new NetworkCredential(userName, password);
+            req.UseBinary = true;
+            req.UsePassive = true;
+            byte[] data = System.IO.File.ReadAllBytes(fileName);
+            req.ContentLength = data.Length;
+            Stream stream = req.GetRequestStream();
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+            FtpWebResponse res = (FtpWebResponse)req.GetResponse();
+            return res.StatusDescription;
+        }
+
+    }
+}
--- Login/FileServer.cs
+++ Login/FileServer.cs
@@ -8,12 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace Login
 {
     public partial class Form2 : Form
     {
+        FileServerConnect fsc = new FileServerConnect();
+
         public Form2()
         {
+
+            fsc.UploadFile("ftp://192.168.2.36/", @"C:\Users\kefid\OneDrive\Bilder\dondacover.png",
+                "ftpserveruser","mygrind400","/abc");
+
             InitializeComponent();
         }
     }
